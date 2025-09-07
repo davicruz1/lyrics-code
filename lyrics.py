1f5b56a47ca3f833a9deec6e95871a928c3dd72a
davi <davicruzramos1@gmail.com>
2025-09-07 18:20:28 -0300

    lyrics.py

--- lyrics.py
+++ lyrics.py
@@ -5,19 +5,7 @@ import sys
 os.system("cls" if os.name == "nt" else "clear")
 
 lyrics = [
-    "I wanna be your vacuum cleaner",
-    "Breathing in your dust",
-    "I wanna be your Ford Cortina",
-    "I will never rust",
-    "I just wanna be yours",
-    "",
-    "I wanna be your raincoat",
-    "For those frequent rainy days",
-    "I wanna be your dreamboat",
-    "When you want to sail away",
-    "Let me be yours",
-    "",
-    "I just wanna be yours..."
+    " "
 ]
 
 def type_line(line, delay=0.05):
@@ -33,3 +21,4 @@ def printLyrics():
 
 if __name__ == "__main__":
     printLyrics()
+
